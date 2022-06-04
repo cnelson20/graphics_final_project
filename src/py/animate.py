48befeeb7cc720e89f14a4c3ab7c5b9ef045c959
cnelson20 <cjnelson2004@gmail.com>
2022-06-04 04:58:34 +0100

    an

--- src/py/animate.py
+++ src/py/animate.py
@@ -2,5 +2,5 @@ import sys, os
 
 basename = sys.argv[1]
 
-os.system('convert -delay 1.67 ' + basename + '_*.ppm ' + basename + '.gif')
+os.system('convert -delay 1.67 anim/' + basename + '_*.ppm ' + basename + '.gif')
 os.system('rm ' + basename + '_*.ppm')
